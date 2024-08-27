d004e01adbbf4dee0a20f44d96566f337ed50072
tanishqawasthi <awasthitanishq03@gmail.com>
2024-08-27 10:12:26 +0530

    nav, exp, socials base components added

---
+++ src/components/elements/Navbar.jsx
@@ -0,0 +1,33 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "../ui/navbar-menu";
+import { cn } from "@/lib/utils";
+
+export default function Navbar() {
+  return (
+    (<div className="relative w-full flex items-center justify-center">
+      <Nav className="top-2" />
+    </div>)
+  );
+}
+
+function Nav({
+  className
+}) {
+  const [active, setActive] = useState(null);
+  return (
+    (<div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
+      <Menu setActive={setActive}>
+        <MenuItem setActive={setActive} active={active} item="About Me">
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Experiences">
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Projects">
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Connect">
+        </MenuItem>
+      </Menu>
+    </div>)
+  );
+}
